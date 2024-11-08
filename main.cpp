90138b460aeb03838f1764e7ced585a15613c2c7
Bhusan <bhusan12sahoo@gmail.com>
2024-11-08 17:22:36 +0530

    Added the logic of selection of your first pokemon
    first oak intro himself then he gives a options to choose the pokemon.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,35 @@
 using namespace std;
 
 
-void selectPokemon()
+int selectPokemon()
 {
-    //logic to select pokemon
-    //i will apply logic in next lesson
+    cout << "Hyyyyyyyyy.... I am Professor Oak.\n";
+    cout << "You can choose one of the following Pokémon:\n";
+    cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+    cout << "Which Pokémon would you like to choose? Enter the number: ";
+
+    int choice;
+    cin >> choice;
+
+
+    if (choice == 1)
+    {
+        cout << "You choose Bulbasaur\n";
+    }
+    else if (choice == 2)
+    {
+        cout << " HMM, You choose Charmander\n";
+    }
+    else if (choice == 3)
+    {
+        cout << "WOW, You have a great choice,Squirtle";
+    }
+    else 
+    {
+        cout << "Sorry, there are only 3 options\nTry again...";
+    }
+
+    return 0;
 }
 
 
@@ -35,6 +60,8 @@ int main() {
     cout << "Each choice you make shapes your path—catch Pokémon,\ntrain hard, and become the very best!Are you ready\nto step into the tall grass and begin your journey?\nYour adventure starts now!Let’s catch 'em all!\n";
     cout << "=======================================================\n";
 
+
+    selectPokemon();
 
     return 0;
 }
