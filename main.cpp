d874fd124a6b9cdc2095780ddfee29cdec14eac4
Bhusan <bhusan12sahoo@gmail.com>
2024-11-08 16:15:09 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string player_name;
+
+    cout << "Enter your name: ";
+    cin >> player_name;
+
+    cout << "        Bonjour<<<<        " << player_name << "       >>>>Bonjour\n\n\n\n";
+
+
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+    cout << "       <<<<Welcome to the world of Pokemon!>>>>" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n\n";
+
+
+    cout << "In this land of adventure, amazing creatures\nawait your discovery.\n";
+    cout << "=======================================================\n";
+    cout << "As a Pokémon Trainer, your journey will be \nfilled with thrilling battles, new friendships,\nand countless challenges.\n";
+
+    cout << "=======================================================\n";
+    cout << "Each choice you make shapes your path—catch Pokémon,\ntrain hard, and become the very best!Are you ready\nto step into the tall grass and begin your journey?\nYour adventure starts now!Let’s catch 'em all!\n";
+    cout << "=======================================================\n";
+
+
+    return 0;
+}
