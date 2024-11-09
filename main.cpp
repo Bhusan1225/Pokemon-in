8d91434f7985f16e257e04752e56eac59229dd4d
Bhusan <bhusan12sahoo@gmail.com>
2024-11-09 05:49:51 +0530

    Story narration changes
    Nothing special

--- main.cpp
+++ main.cpp
@@ -4,18 +4,21 @@ using namespace std;
 int main() {
     string player_name;
 
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    cout << "       <<<<Welcome to the world of Pokemon!>>>>" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
+
+    cout << "Professor Oak: Hello there! Welcome to the world of Pokemon!\n";
+    cout << "Professor Oak: My name is Oak. People call me the Pokemon "
+        "Professor!\n";
+    cout << "Professor Oak: But enough about me. Let's talk about you!\n";
     cout << "Enter your name: ";
+    cout << "Professor Oak: First, tell me, what’s your name?\n";
     cin >> player_name;
 
-    cout << "        Bonjour<<<<        " << player_name << "       >>>>Bonjour\n\n\n\n";
-
-
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-
-    cout << "       <<<<Welcome to the world of Pokemon!>>>>" << endl;
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n\n";
-
-
+    cout << "Professor Oak: Ah, " << player_name
+        << "! What a fantastic name!\n";
+   
     cout << "In this land of adventure, amazing creatures\nawait your discovery.\n";
     cout << "=======================================================\n";
     cout << "As a Pokémon Trainer, your journey will be \nfilled with thrilling battles, new friendships,\nand countless challenges.\n";
@@ -23,6 +26,9 @@ int main() {
     cout << "=======================================================\n";
     cout << "Each choice you make shapes your path—catch Pokémon,\ntrain hard, and become the very best!Are you ready\nto step into the tall grass and begin your journey?\nYour adventure starts now!Let’s catch 'em all!\n";
     cout << "=======================================================\n";
+
+    cout << "Professor Oak: You must be eager to start your adventure. But "
+        "first, you’ll need a Pokemon of your own!\n";
 
 
     return 0;
