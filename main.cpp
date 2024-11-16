14f1f06e64dbd95fa025d266366fc1431b8d9b89
Bhusan <bhusan12sahoo@gmail.com>
2024-11-16 12:42:57 +0530

    Set the stage for combat_assignment submission

--- main.cpp
+++ main.cpp
@@ -8,6 +8,26 @@
 #include <limits> // Include this header to use std::numeric_limits
 #include <string>
 
+
+void battle(Pokemon& playerPokemon, Pokemon& wildPokemon) {
+	cout << "A wild " << wildPokemon.name << " appeared!\\n";
+
+	while (!playerPokemon.isFainted() && !wildPokemon.isFainted()) {
+		playerPokemon.attack(wildPokemon); // Player attacks first
+
+		if (!wildPokemon.isFainted()) {
+			wildPokemon.attack(playerPokemon); // Wild Pokémon attacks back
+		}
+	}
+
+	if (playerPokemon.isFainted()) {
+		cout << playerPokemon.name << " has fainted! You lose the battle.\\n";
+	}
+	else {
+		cout << "You defeated the wild " << wildPokemon.name << "!\\n";
+	}
+}
+
 int main() {
 
 	// Continue with the main flow of the game
