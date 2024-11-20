85b01fd0f44ed7b863a6863b3b66396a0c054512
Bhusan <bhusan12sahoo@gmail.com>
2024-11-20 09:41:33 +0530

    made changes in main and convert the soft classes to pointer class

--- main.cpp
+++ main.cpp
@@ -15,21 +15,24 @@ int main() {
 	cout << "        Unite the Worlds, Unleash the Legends." << endl;
 	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n";
 	// Continue with the main flow of the games
-	ProfessorOak professor("Professor Oak");
-	Player player;
+	ProfessorOak* professor = new ProfessorOak("Professor Oak");
+	Player* player = new Player();
 
 	// Greet the player and offer Pokemon choices
-	professor.greetPlayer(player);
-	professor.offerPokemonChoices(player);
+	professor->greetPlayer(player);
+	professor->offerPokemonChoices(player);
 
 	// Explain the main quest
-	professor.explainMainQuest(player);
+	professor->explainMainQuest(player);
 
 	// Start the main game loop
-	Game game;
-	game.gameLoop(player);
+	Game* game = new Game();
+	game->gameLoop(player);
 
 	//checking branch main 
+	delete professor;
+	delete player;
+	delete game;
 
 	return 0;
 }
